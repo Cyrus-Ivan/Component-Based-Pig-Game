95a2d93a5cd7ad1d67449e21cdc3669281cbd157
Cyrus Ivan <cyrusivancruz2002@gmail.com>
2024-07-03 10:52:29 +0800

    tapos na yung game with keyboardevent pa

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 'use strict';
 
 const numberOfPlayers = 2;
-
-//dice image
-const dice = document.getElementById('dice');
-
-// current total of active playyer
-let activeCurrent = 0;
-// players score tally
-let player1Score = 0;
-let player2Score = 0;
-
-// current player manipulator
+const playersArray = [];
 let playerNumber = 0;
-let currentPlayer = document.querySelector(`.player--${playerNumber}`);
+let isDone = false;
+
+function initializePlayers() {
+  for (let i = 0; i < numberOfPlayers; i++) {
+    playersArray[i] = {
+      player: `player--${i}`,
+      score: 0,
+      current: 0,
+    };
+  }
+}
+initializePlayers();
+
 function switchActivePlayer() {
+  let currentPlayer = document.querySelector(
+    `.${playersArray.at(playerNumber).player}`
+  );
   // current player's active status gets revoked
   currentPlayer.classList.toggle('player--active');
   // playerNumber gets switched to the next player
   playerNumber = playerNumber + 1 < numberOfPlayers ? playerNumber + 1 : 0;
   // next Player is now assigned as current player and active status is toggled on.
-  currentPlayer = document.querySelector(`.player--${playerNumber}`);
+  currentPlayer = document.querySelector(
+    `.${playersArray.at(playerNumber).player}`
+  );
   currentPlayer.classList.toggle('player--active');
 }
 
@@ -33,39 +40,74 @@ const holdButton = document.querySelector('.btn--hold');
 newGameButton.addEventListener('click', reset);
 rollDiceButton.addEventListener('click', roll);
 holdButton.addEventListener('click', hold);
-document.addEventListener('keydown', keyPressHandler);
+document.addEventListener('keypress', keyPressHandler);
 
 // the listeners corresponding functions
 function reset() {
-  player1Score = 0;
-  player2Score = 0;
-  document.querySelectorAll('.current').textContent = 0;
-  DocumentTimeline.querySelectorAll('.player').textContent = 0;
+  isDone = false;
+  initializePlayers();
+  document
+    .querySelectorAll('.player')
+    .forEach(element => element.classList.remove('player--winner'));
+  document
+    .querySelectorAll('.current-score')
+    .forEach(element => (element.textContent = 0));
+  document
+    .querySelectorAll('.score')
+    .forEach(element => (element.textContent = 0));
 }
+
 function roll() {
-  console.log('roll');
+  if (isDone) return true;
 
   let numberRolled = Math.floor(Math.random() * 6 + 1);
-  dice.src = `dice-${numberRolled}.png`;
+  document.getElementById('dice').src = `dice-${numberRolled}.png`;
 
-  document.getElementById(`current--${playerNumber}`).textContent;
+  if (numberRolled === 1) {
+    playersArray.at(playerNumber).current = 0;
+    hold();
+  } else {
+    playersArray.at(playerNumber).current += numberRolled;
+    document.getElementById(`current--${playerNumber}`).textContent =
+      playersArray.at(playerNumber).current;
+  }
 }
+
 function hold() {
-  console.log('\nhold');
-  addToTotal();
-  winCheck();
-  switchActivePlayer();
+  if (isDone) return true;
+
+  playersArray.at(playerNumber).score += playersArray.at(playerNumber).current;
+  document.getElementById(`score--${playerNumber}`).textContent =
+    playersArray.at(playerNumber).score;
+
+  playersArray.at(playerNumber).current = 0;
+  document.getElementById(`current--${playerNumber}`).textContent = 0;
+  if (!winCheck()) switchActivePlayer();
 }
+
 function keyPressHandler(KeyboardEvent) {
-  if (KeyboardEvent.key === 'Escape') reset();
-  else if (KeyboardEvent.key === 'Enter') roll();
-  else if (KeyboardEvent.key === ' ') hold();
+  switch (KeyboardEvent.key) {
+    case 'Escape':
+      reset();
+      break;
+    case ' ':
+      hold();
+      break;
+    case '+':
+      roll();
+      break;
+  }
 }
 
 // other functions called by the previous primary functions
 
-function addToTotal() {}
-
 function winCheck() {
-  console.log('check');
+  if (playersArray.at(playerNumber).score >= 100) {
+    document
+      .querySelector(`.${playersArray.at(playerNumber).player}`)
+      .classList.add('player--winner');
+    isDone = true;
+    return true;
+  }
+  return false;
 }
